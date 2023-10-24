a8a643d43388925b29aa044cdca0e75d763713b9
Logan-Fullerton <144399401+Logan-Fullerton@users.noreply.github.com>
2023-10-24 16:11:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+var currentTime = dayjs().format('H')
+const saveButtons= document.querySelectorAll('.saveBtn')
+const textAreas= document.querySelectorAll('.description')
+for (const saveBtn of saveButtons) {
+   saveBtn.addEventListener('click',function(e) {
+      let userInput=saveBtn.previousElementSibling.value
+      let timeBlock=saveBtn.parentElement.id
+      localStorage.setItem(timeBlock, userInput)
+   })
+}
+for (const textarea of textAreas) {
+   let timeBlock=textarea.parentElement.id
+   if(localStorage.getItem(timeBlock)){
+      textarea.textContent=localStorage.getItem(timeBlock)
+   }
+}
+var rowElements = document.querySelectorAll(".row");
+
+rowElements.forEach(function(rowElement) {
+  var divId = rowElement.id;
+  console.log(divId);
+  
+  
+  var divElement = document.querySelectorAll(".row");
+  
+  var divId = rowElement.id;
+  console.log(typeof currentTime , typeof divId)
+  if (currentTime < divId) {
+    divElement.classList.add("future");
+  
+  if(currentTime> divId.textContent)
+   divElement.classList.add("past");
+  
+  else
+   divElement.classList.add('present');
+  }
 });
+
